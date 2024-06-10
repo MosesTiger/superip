6eaa1933a2ba624bf92e831b7f3263616758f0bb
ehdgnsl01 <gnsl505@naver.com>
2024-06-10 09:19:41 +0900

    update 240610

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,17 +8,20 @@ import "../Layout/Main.css";
 const Main = () => (
   <div className="main-page">
     <header className="header">
+      <br></br>
       <div className="logo">Logo</div>
       <input type="text" className="search-bar" placeholder="영화 검색..." />
       <button className="login-button">로그인</button>
     </header>
-    <h1>Create Story</h1>
-    <nav className="nav-bar">
-      <span>How</span>
-      <span>QnA</span>
-      <span>ToS</span>
-      <span>Setting</span>
-    </nav>
+    <div className="title">
+      <h1>Create Story</h1>
+      <nav className="nav-bar">
+        <span>How</span>
+        <span>QnA</span>
+        <span>ToS</span>
+        <span>Setting</span>
+      </nav>
+    </div>
     <div className="card-container">
       <CreateStory />
       <PredictHit />
